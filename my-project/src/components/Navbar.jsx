a5ec7ab50b833318844809be63b0eab23aaa40e9
AmaanNakhwa <amaannakhwa23@gmail.com>
2024-07-02 17:45:15 +0530

    Improved Navbar

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
             {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
         </div>
 
-        <div className={nav ? 'z-10 text-gray-300 fixed h-full left-0 top-0 w-[60%] border-r-gray-900 bg-[#d1d5db] dark:bg-[#121212]  ease-in-out duration-500'
+        <div className={nav ? 'z-10  fixed h-full left-0 top-0 w-[60%] border-r-gray-900 bg-[#d1d5db] dark:bg-[#121212]  ease-in-out duration-500'
             : 'fixed left-[-100%]'}>
         <h1 className='text-3xl primary-color m-4'>A. Nakhwa</h1>
         <ul className='p-8 text-2xl'>
